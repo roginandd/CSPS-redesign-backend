9301be7292b5498d7c4954ccdeb6cce8fb88527f
roginand <roginandv@gmail.com>
2025-10-03 04:11:22 +0800

    feat(domain): add EventType and EventStatus enums for event categorization

---
+++ src/main/java/org/csps/backend/domain/enums/EventStatus.java
@@ -0,0 +1,8 @@
+package org.csps.backend.domain.enums;
+
+public enum EventStatus {
+    UPCOMING,
+    ACTIVE,
+    FINISHED,
+    CANCELLED
+}
---
+++ src/main/java/org/csps/backend/domain/enums/EventType.java
@@ -0,0 +1,11 @@
+package org.csps.backend.domain.enums;
+
+public enum EventType {
+    WORKSHOP,
+    SEMINAR,
+    CONFERENCE,
+    PARTY,
+    TUTORIAL,
+    COMPETITION,
+    OTHER
+}
